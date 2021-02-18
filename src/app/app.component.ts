4472ed0af7c3e540ca3b3e109365e1d0e7302265
prakharomer <40533274+prakharomer@users.noreply.github.com>
2021-02-18 22:24:49 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component, VERSION } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthStore} from './services/auth.store';
+
+
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements  OnInit {
+
+    constructor(public auth: AuthStore) {
+
+    }
+
+    ngOnInit() {
+
+
+    }
+
+  logout() {
+        this.auth.logout();
+
+  }
+
 }
